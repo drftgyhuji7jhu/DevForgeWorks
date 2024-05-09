c41a00489f5efc7a9aef1fb7764322ed092e39b7
drftgyhuji7jhu <gs1sdas22da@hotmail.com>
2024-05-09 13:21:50 +0700

    update project

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const initializeArrayWithRange = (end, start = 0) =>
-  Array.from({ length: end - start }, (_, i) => i + start);
+function combinationSum4(nums, target) {
+  const dp = new Array(target + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i <= target; i++) {
+    for (const num of nums) {
+      if (i >= num) {
+        dp[i] += dp[i - num];
+      }
+    }
+  }
+  return dp[target];
+}
