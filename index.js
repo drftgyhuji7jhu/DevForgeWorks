9c8492641892f35f343787c40bfdf6af924d3e15
drftgyhuji7jhu <gs1sdas22da@hotmail.com>
2024-05-12 10:48:25 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-const mergeSort = (arr) => {
-  if (arr.length <= 1) {
-    return arr;
+const gcdRecursive = (a, b) => {
+  if (b === 0) {
+    return a;
   }
-  const mid = Math.floor(arr.length / 2);
-  const left = mergeSort(arr.slice(0, mid));
-  const right = mergeSort(arr.slice(mid));
-  return merge(left, right);
-};
-const merge = (left, right) => {
-  let result = [];
-  let leftIndex = 0;
-  let rightIndex = 0;
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      result.push(left[leftIndex]);
-      leftIndex++;
-    } else {
-      result.push(right[rightIndex]);
-      rightIndex++;
-    }
-  }
-  return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  return gcdRecursive(b, a % b);
 };
